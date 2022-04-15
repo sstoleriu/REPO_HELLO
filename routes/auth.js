ed7be8551af81ae05d8e9e097ac8c9c3ef1f97d3
Stoleriu Stefan <stefan.stoleriu@student.tuiasi.ro>
2022-04-15 17:27:48 +0300

    Initial commit

---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const user = require("../models/User");
+//REGISTER
+
+router.post("/register", async (req, res) => {
+    const newUser = new user({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    });
+
+    try {
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser)
+    } catch (err) {
+        res.status(500).json(err);
+        console.log("aoleu")
+    }
+});
+
+module.exports = router;
